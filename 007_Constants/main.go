1a0626c179a5a04a02b21e146dc55749e7f57416
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-08-12 09:33:13 +0530

    Update main.go

--- 007_Constants/main.go
+++ 007_Constants/main.go
@@ -1,13 +1,18 @@
 package main
 
+import "fmt"
 
-	// Note
-	// We can declare the variable outside the func main() function
+// Note
+// We can declare the variable outside the func main() function
 
-	// Ex
-	
+// Ex
+var age int = 30
+
+const className string = "Golang"
 
 func main() {
+
+	fmt.Println("Age is : = ", age, "Class Name is : = ", className)
 
 	// Constants
 
