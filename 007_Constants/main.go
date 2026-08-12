d4859f1b000fe5affcb4e2b20bbc4bd711904122
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-08-12 09:29:19 +0530

    Update main.go

--- 007_Constants/main.go
+++ 007_Constants/main.go
@@ -4,8 +4,13 @@ func main() {
 
 	// Constants
 
-	
 	const name = "goLang"
-	name = "Script"
+
+	// Can't Redecalre the constant values
+	// name = "Script"
+	/*
+		cannot assign to name (neither addressable nor a map index expression)compilerUnassignableOperand
+		const name untyped string = "goLang"
+	*/
 
 }
