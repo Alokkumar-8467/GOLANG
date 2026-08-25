1d2a6ab3b6f1d13d6de868708f7e3ecd13e638d8
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-08-25 09:36:38 +0530

    Update main.go

--- 013-Maps/main.go
+++ 013-Maps/main.go
@@ -30,4 +30,7 @@ func main() {
 	fmt.Println(n["roll"], n["kola"])
 	// Output = 12 0
 
+	// Now see we initalize roll in mpa not kola but wehn we access these two value, then we get 12 and 0 in map.
+	// kola is not present in map so it give 0 because our KEY-VALUE pair is string-int.
+
 }
