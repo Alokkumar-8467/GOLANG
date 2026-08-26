a12442fc6bd0c3c2ff1f87f45c391e5d2f79d9f4
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-08-26 17:02:27 +0530

    Update main.go

--- 015_Func/main.go
+++ 015_Func/main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+/*
+In this line "func add(a int, b int) int {"
+
+this line represent the input taken by function add(a int, b int) and type is int.
+
+but this int { define that there is a return type and must be int.
+
+*/
+
 func add(a int, b int) int {
 	return a + b
 }
