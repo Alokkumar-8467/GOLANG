0b3927a8665964fd11351d3e08e575bcdfc1f85f
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-08-26 15:51:18 +0530

    Update main.go

--- 014_Range/main.go
+++ 014_Range/main.go
@@ -7,8 +7,13 @@ func main() {
 
 	nums := []int{6, 7, 8}
 
+	// printing ARRAY by using FOR loop
 	for i := 0; i < len(nums); i++ {
 		fmt.Println(nums[i])
 	}
+
+	// Output = 6 7 8
+
+	// Printing Array by using RANGE
 
 }
