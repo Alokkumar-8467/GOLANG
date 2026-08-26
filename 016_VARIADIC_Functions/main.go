7328b38d56320d8bd10e9f133e0303a78c582369
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-08-26 17:53:09 +0530

    Update main.go

--- 016_VARIADIC_Functions/main.go
+++ 016_VARIADIC_Functions/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main(){
-  
+import "fmt"
+
+
+
+
+func main() {
+
+	// Variadic Function in fmt.Println()
+	fmt.Println(1, 2, 3, 4, 5, 6, 7, "Hello")
+	// In Variadic function we can pass any number of parameters, basically there is no limit for that.
+
 }
