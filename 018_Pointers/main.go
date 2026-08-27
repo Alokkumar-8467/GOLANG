019fce699e515302d2c04309a96ded4b1bcbf0c6
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-08-27 17:41:57 +0530

    Update main.go

--- 018_Pointers/main.go
+++ 018_Pointers/main.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
+func changeNum(num int) {
+	num = 5
+	fmt.Println("In changeNum", num)
+}
+
 func main() {
+
+	num := 1
+
+	changeNum(num)
+	fmt.Println("After changeNum in main", num)
 
 }
