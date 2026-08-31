b6faa099b8b977a3f53c0bcf9d60e783ab564793
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-08-31 09:32:49 +0530

    Create main.go

---
+++ 019_Struct/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// Structs are basically custom Data Structure
+
+// Order Struct
+
+type order struct {
+	id        string
+	amount    float32
+	status    string
+	createdAt time.Time
+}
+
+func main() {
+
+
+
+}
