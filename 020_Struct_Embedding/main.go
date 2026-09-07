b6f5cba3f33f3bcebaf6432c542ca998da82ed21
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-07 11:23:12 +0530

    Update main.go

--- 020_Struct_Embedding/main.go
+++ 020_Struct_Embedding/main.go
@@ -7,7 +7,16 @@ import (
 
 // We can embed struct in another struct to use the fields of the embedded struct in the parent struct
 
-// Like in Order struct we can
+// Like in Order struct we can embed the Customer struct to use the fields of Customer in Order struct
+
+type customer struct {
+	name  string
+	phone string
+}
+
+type order struct {
+	id        string
+	amount    float32
 	status    string
 	createdAt time.Time
 	customer  // Embedded struct
@@ -20,3 +29,22 @@ func main() {
 		amount: 50.00,
 		status: "received",
 		customer: customer{
+			name:  "John Doe",
+			phone: "1234567890",
+		},
+	}
+
+	fmt.Println("Order Struct", newOrder)
+	fmt.Println("Order Struct", newOrder.customer)
+	fmt.Println("Customer Name:", newOrder.customer.name)
+	fmt.Println("Customer Phone:", newOrder.customer.phone)
+	// Output = Order Struct {1 50 received {0 0 <nil>} {John Doe 1234567890}}
+
+	/*
+		Order Struct {1 50 received {0 0 <nil>} {John Doe 1234567890}}
+		Order Struct {John Doe 1234567890}
+		Customer Name: John Doe
+		Customer Phone: 1234567890
+	*/
+
+}
