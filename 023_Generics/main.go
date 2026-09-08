fce30acf71ff8540fcd1a53fd7cdf900f36319d0
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-08 10:45:19 +0530

    Update main.go

--- 023_Generics/main.go
+++ 023_Generics/main.go
@@ -1,6 +1,5 @@
 package main
 
-
 import "fmt"
 
 func printSlice(items []int) {
@@ -25,6 +24,16 @@ func printGenericSlice[T any](items []T) {
 	}
 }
 
+/*
+comparable is an interface that is implemented by all comparable types (booleans, numbers, strings, pointers, channels, arrays of comparable types, structs whose fields are all comparable types). The comparable interface may only be used as a type parameter constraint, not as the type of a variable.
+*/
+func printGenericComparableSlice[T comparable](items []T) {
+	// In place of any we can use interface{} also, but any is more readable and easy to understand.
+	for _, item := range items {
+		fmt.Println(item)
+	}
+}
+
 // Now i want to scope the type so that i can use only int and string in the generic function, so i will create a type constraint for that.
 
 func printGenericScopedSlice[T int | string](items []T) {
@@ -34,6 +43,15 @@ func printGenericScopedSlice[T int | string](items []T) {
 	}
 }
 
+// Now Stack
+type stack struct {
+	elements []int
+}
+
+// Now i want to use this stack for string also, so i will create a generic stack.
+type genericStack[T any] struct {
+	elements []T
+}
 
 func main() {
 
@@ -57,7 +75,6 @@ func main() {
 	fmt.Println("Printing the int Slice by using generics")
 	fmt.Println("*************")
 	// Now Print the string Slice using generics
-	
 	printGenericSlice(names)
 	fmt.Println("Printing the string Slice by using generics")
 
@@ -76,5 +93,31 @@ func main() {
 	// printGenericScopedSlice(decimalNumber)
 
 	// Same for other data Types like float64, bool, etc. we can not use them in the scope generics function, beacuse we only declare INT and STRING in the scope generics function. So, if we try to use any other type then it will give an error.
+
+	printGenericComparableSlice(nums)
+	fmt.Println("Printing the int Slice by using comparable generics")
+	fmt.Println("*************")
+	printGenericComparableSlice(names)
+	fmt.Println("Printing the string Slice by using comparable generics")
+
+	fmt.Println("*************")
+
+	fmt.Println("Now Stack")
+
+	// Now Stack
+	myStack := stack{
+		elements: []int{1, 2, 3},
+	}
+	fmt.Println("Basic Stack value", myStack)
+
+	// Now Generic Stack
+	fmt.Println("*************")
+
+	fmt.Println("Now Stack")
+	myGenericStack := genericStack[string]{
+		elements: []string{"GOLANG", "JS", "SQL"},
+	}
+
+	fmt.Println("Generic Stack value", myGenericStack)
 
 }
