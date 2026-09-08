ea880d81c10dff1f30dcd4ec8a84c58df8fe5d0d
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-08 14:12:21 +0530

    Update main.go

--- 024_GoRoutine/main.go
+++ 024_GoRoutine/main.go
@@ -4,3 +4,16 @@ import (
 	"fmt"
 	"time"
 )
+
+
+func task(id int) {
+	fmt.Println("Doing task", id)
+}
+
+func brewCoffee() {
+	fmt.Println("Brewing coffee...")
+}
+
+func toastBread() {
+	fmt.Println("Toasting bread...")
+}
