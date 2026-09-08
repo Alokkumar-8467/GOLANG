2a70f078de25e985437309b1738a567488aa4eaf
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-08 15:08:02 +0530

    Update main.go

--- 026-Channels/main.go
+++ 026-Channels/main.go
@@ -1,3 +1,9 @@
 package main
 
 // When we have multiple goroutines running concurrently, we need a way to communicate between them. Channels provide a way for one goroutine to send data to another goroutine. Channels are a powerful feature of Go that allow us to build concurrent programs that are easy to reason about.
+
+
+func main() {
+
+	// How to create a channel in Go?
+	messageChan := make(chan string)
