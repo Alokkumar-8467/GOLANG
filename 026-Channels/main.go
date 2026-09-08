e597d9f4cfcca40d7529141ce5666e835f05bde5
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-08 15:57:36 +0530

    Update main.go

--- 026-Channels/main.go
+++ 026-Channels/main.go
@@ -2,6 +2,8 @@ package main
 
 // When we have multiple goroutines running concurrently, we need a way to communicate between them. Channels provide a way for one goroutine to send data to another goroutine. Channels are a powerful feature of Go that allow us to build concurrent programs that are easy to reason about.
 
+// DEADLOCK
+// A deadlock is a situation where a program (or a set of goroutines/threads) gets stuck forever, because everyone involved is waiting on something that will never happen.
 
 func main() {
 
