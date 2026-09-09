d04f50f2deffbee4b02a42a4db85cb0f44af24bc
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-09 14:38:11 +0530

    Update main.go

--- 026-Channels/main.go
+++ 026-Channels/main.go
@@ -39,6 +39,8 @@ func task(done chan bool) {
 // 6. Email sender by CHANNEL
 func emailSender(emailChan chan string, emailDone chan bool) {
 	defer func() { emailDone <- true }()
+	for email := range emailChan {
+		fmt.Println("Sending email to", email)
 
 func main() {
 
