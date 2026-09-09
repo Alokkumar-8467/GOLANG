7a22b82b4a9d8610af7c75a354f0dbb7a343a3e2
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-09 13:52:50 +0530

    Update main.go

--- 026-Channels/main.go
+++ 026-Channels/main.go
@@ -30,6 +30,8 @@ func sum(result chan int, num1 int, num2 int) {
 
 // 5. We can do waitGroup functionality by using CHANNEL also.
 func task(done chan bool) {
+		// Here we use difer function, it run when all the take is completed. If function give error then also it run.
+	defer func() { done <- true }()
 
 
 func main() {
