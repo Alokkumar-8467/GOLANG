de898277ea6bb711c4ac71032266112399d183d6
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-10 11:27:26 +0530

    Update files.go

--- 028_Files/files.go
+++ 028_Files/files.go
@@ -7,14 +7,14 @@ import (
 
 func main() {
 
-	// 1. File Information **********
+	// 1. File Information ****************************************
 	f, err := os.Open("example.txt")
 	// The os.Open() return two things.
 	// It return two things.
 	// 1. File-Object which is pointer
 	// 2. error
 
-		// Now i go when error comes we need to handle it or return it, we can't thow that error in GOLANG
+	// Now i go when error comes we need to handle it or return it, we can't thow that error in GOLANG
 	// So,
 	if err != nil {
 		// Log the error
@@ -23,7 +23,7 @@ func main() {
 		panic(err)
 	}
 
-		// We handle the error Now file_Object turn
+	// We handle the error Now file_Object turn
 	// In file we have lots of options to use like
 	/*
 		f.Stat()
@@ -31,57 +31,62 @@ func main() {
 		f.Read()
 		f.ReadAt()
 	*/
+
 	/*
 		Now take f.Stat(), same it takes os.FileInfo, and error.
 	*/
 
-		fileInfo, err := f.Stat()
+	fileInfo, err := f.Stat()
 	// again same for this also.
 	// Now i go when error comes we need to handle it or return it, we can't thow that error in GOLANG
 	// So,
-
-		if err != nil {
+	if err != nil {
 		// Log the error
 
 		// Panic the Program
 		panic(err)
 	}
 
-		// Same with fileInfo, we have various methods with file info like
+	// Same with fileInfo, we have various methods with file info like
 	/*
 		fileInfo.Name()
 		fileInfo.Mode()
 		fileInfo.Size()
 	*/
 
-		fmt.Println("FileName = ", fileInfo.Name())
+	fmt.Println("FileName = ", fileInfo.Name())
 	fmt.Println("FileSize = ", fileInfo.Size())
 	fmt.Println("File Modified Time Last Time = ", fileInfo.ModTime())
 	fmt.Println("", fileInfo.Sys())
 	fmt.Println("File or Folder = ", fileInfo.IsDir())
 	fmt.Println("File Permissions = ", fileInfo.Mode())
 
+	/* Output =
+	FileName =  example.txt
+	FileSize =  12
+	File Modified Time Last Time =  2026-09-10 10:13:01.5497977 +0530 IST &{32 {3635673320 31277278} {3697597689 31277278} {3697597689 31277278} 0 12}
+	File or Folder =  false
+	File Permissions =  -rw-rw-rw-
+	*/
 
-	
 	// 2. File Read ****************************************
 
-		// 2.1 File Read by classical way ****************************************
+	// 2.1 File Read by classical way ****************************************
 
 	f1, err := os.Open("example.txt")
 	if err != nil {
 		panic(err)
 	}
 
-		// When we open a file, it must need to be closed.
+	// When we open a file, it must need to be closed.
 	defer f1.Close()
 
-	
 	// Now read the file content.
 	// So basically when we read any file we need to store that file data into buffer. And buffer is Array of bytes.
 
 	buf := make([]byte, fileInfo.Size())
 
-		d, err := f.Read(buf)
+	d, err := f.Read(buf)
 	if err != nil {
 		panic(err)
 	}
@@ -105,13 +110,13 @@ func main() {
 	Data from file read =  12 G
 	*/
 
-		// 2.2 File Read by ReadFile method ****************************************
+	// 2.2 File Read by ReadFile method ****************************************
 
 	f2, err := os.ReadFile("example.txt")
 	if err != nil {
 		panic(err)
 	}
-	
+
 	fmt.Println("Read file data by ReadFile Method = ", string(f2))
 
 	// Output = Read file data by ReadFile Method =  Hello GOLANG
