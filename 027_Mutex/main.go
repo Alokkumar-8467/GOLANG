303a972118fdd73f69fef1ed8869d9ad1a0c28ad
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-10 10:06:55 +0530

    Update main.go

--- 027_Mutex/main.go
+++ 027_Mutex/main.go
@@ -6,3 +6,8 @@ import (
 )
 
 // When we do multiThreading then to avoide the raise condition we use MUTEX
+
+/* What is raise condition
+When multiple processes use the same or single resources then modification on that resources is bot atomic.
+There is a hight chancet that if Process 1 chance the resource then process 2 also change the resource so it create conflict in the final result.
+*/
