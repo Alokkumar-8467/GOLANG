d2a66856c3d596b2ec142072a9e6708992cf90d7
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-10 10:08:43 +0530

    Update main.go

--- 027_Mutex/main.go
+++ 027_Mutex/main.go
@@ -43,4 +43,11 @@ func (p *post) inc(wg *sync.WaitGroup) {
 func main() {
 
 	var wg sync.WaitGroup
+		myPost := post{views: 0}
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go myPost.inc(&wg)
+	}
+
 
