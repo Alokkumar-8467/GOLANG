3a1216fc674654264ef56a297ed19d3851898946
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-10 10:07:03 +0530

    Update main.go

--- 027_Mutex/main.go
+++ 027_Mutex/main.go
@@ -11,3 +11,9 @@ import (
 When multiple processes use the same or single resources then modification on that resources is bot atomic.
 There is a hight chancet that if Process 1 chance the resource then process 2 also change the resource so it create conflict in the final result.
 */
+
+type post struct {
+	views int
+	// Here we add mutex in views resource.
+	mu sync.Mutex
+}
