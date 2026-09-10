1ffac049a0fdb7004742e4ae03a168d64ee0463f
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-10 20:21:59 +0530

    Update credentials.go

--- 029_Packages/auth/credentials.go
+++ 029_Packages/auth/credentials.go
@@ -1,3 +1,6 @@
 package auth
 
 import "fmt"
+/*
+I have a function loginWithCredentials Now,
+1. If Function start with small letter then we only use that particular function on that folder. In this loginWithCredentials() used only on /auth folder.
