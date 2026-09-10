ead6e89ab36a32c83f31177ecac34045a551a235
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-10 10:49:47 +0530

    Update files.go

--- 028_Files/files.go
+++ 028_Files/files.go
@@ -4,3 +4,12 @@ import (
 	"fmt"
 	"os"
 )
+
+func main() {
+
+	// 1. File Information **********
+	f, err := os.Open("example.txt")
+	// The os.Open() return two things.
+	// It return two things.
+	// 1. File-Object which is pointer
+	// 2. error
