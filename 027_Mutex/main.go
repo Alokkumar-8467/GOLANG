c9bb66c2ad910e007eaacd143f5787d89d5f96c4
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-10 10:08:07 +0530

    Update main.go

--- 027_Mutex/main.go
+++ 027_Mutex/main.go
@@ -33,3 +33,6 @@ func (p *post) inc(wg *sync.WaitGroup) {
 
 		// So we move the p.mu.Unlock() to defer() function.
 	//  As we know that either the function run or give error or any deadlock condition the defer function run at END of that function.
+
+		// Never Lock the Whole function or logic by MUTEX, it's a bad practice
+	// Only lock that particular line that perform modification.
