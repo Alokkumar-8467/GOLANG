e51dff129fab4754e037eafd5edf4ab5af3a6ced
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-11 23:55:41 +0530

    Update main.go

--- 029_Packages/main.go
+++ 029_Packages/main.go
@@ -11,6 +11,11 @@ import (
 // Command for custom packages
 // go mod init github.com/alok/podcast
 
+// command for third party packages
+// go get "package url"
+// GOLANG only compile those file which have change not other files.
+
+
 func main() {
 	auth.LoginWithCredentials("alok", "kola")
 	session := auth.GetSession()
