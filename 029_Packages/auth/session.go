484b0bf027ccd418f30e610681a2b14c48fa1d61
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-11 23:53:52 +0530

    Update session.go

--- 029_Packages/auth/session.go
+++ 029_Packages/auth/session.go
@@ -2,6 +2,10 @@ package auth
 
 // now see this extractSession() is private but GetSession() is not private.
 
+func extractSession() string {
+	return "Loggedin"
+}
+
 
 func GetSession() string {
 	return "loggedin"
