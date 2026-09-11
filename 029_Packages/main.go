d6f14a9ea1ef56f23db44f7eba23651282fccd46
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-11 23:57:09 +0530

    Update main.go

--- 029_Packages/main.go
+++ 029_Packages/main.go
@@ -15,17 +15,19 @@ import (
 // go get "package url"
 // GOLANG only compile those file which have change not other files.
 
-
 func main() {
 	auth.LoginWithCredentials("alok", "kola")
 	session := auth.GetSession()
 	fmt.Println("session = ", session)
+
 	user := user.User{
 		Email: "john@email.com",
 		Name:  "John Deo",
 	}
+
 	fmt.Println(user.Email)
 	fmt.Println(user.Name)
+
 	color.Green(user.Email)
 
 	// In GOLANG there are various packages but if we want to install third party packages then we can do that also by using thirt part packages library
