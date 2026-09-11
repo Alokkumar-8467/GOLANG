f15f14792842842d576fc52c54a6f9c1e34292c7
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-11 23:58:14 +0530

    Update user.go

--- 029_Packages/user/user.go
+++ 029_Packages/user/user.go
@@ -2,3 +2,5 @@ package user
 // Same thing if we declare with small alphabrt lke "user" then only used for this package auth.
 // But if we use "User" then we can use this as global on in whole project.
 
+type User struct {
+	Email string
