9181807cc931e3c628752bda9709bab65265f7d2
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-11 23:58:04 +0530

    Update user.go

--- 029_Packages/user/user.go
+++ 029_Packages/user/user.go
@@ -1,1 +1,4 @@
 package user
+// Same thing if we declare with small alphabrt lke "user" then only used for this package auth.
+// But if we use "User" then we can use this as global on in whole project.
+
