fd48900d3ec06f888ca48a59d21b225e0f93c944
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-17 13:37:04 +0530

    Update main.go

--- 031_ADV_GO_LANG/01_Functions/main.go
+++ 031_ADV_GO_LANG/01_Functions/main.go
@@ -9,7 +9,20 @@ func add(a int, b int) int {
 func SumAndProduct(a int, b int) (int, int) {
 	sum := a + b
 	product := a * b
-
 	return sum, product
+}
+
+func main() {
+
+	sum1 := add(10, 20)
+
+	fmt.Println("SUM1 answer is = ", sum1)
+
+	// Output = SUM1 answer is =  30
+
+	s, p := SumAndProduct(12, 34)
+
+	fmt.Println("Sum and Product result = ", s, p)
+	// Output = Sum and Product result =  46 408
 
 }
