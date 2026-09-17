ff91def6221a1f662a1036427e8df6d10879ca66
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-17 14:55:16 +0530

    Update main.go

--- 031_ADV_GO_LANG/03_Variadic_Function/main.go
+++ 031_ADV_GO_LANG/03_Variadic_Function/main.go
@@ -19,3 +19,17 @@ func anyType(nums ...any) int {
 	fmt.Println(nums...)
 	return len(nums)
 }
+
+func main() {
+
+	// Variadic Function in fmt.Println()
+	fmt.Println(1, 2, 3, 4, 5, 6, 7, "Hello")
+	// In Variadic function we can pass any number of parameters, basically there is no limit for that.
+
+	result := sum(1, 2, 3, 4, 5, 6, 7, 8)
+	fmt.Println(result)
+
+	finalResult := anyType(1, 3, 5, 6, "kola", 345, true, false, "alok")
+	fmt.Println(finalResult)
+
+}
