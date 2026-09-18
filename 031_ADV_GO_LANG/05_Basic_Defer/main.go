d95658434356c281d5ce0d1bcbbe5c83ac57094f
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-18 17:17:30 +0530

    Update main.go

--- 031_ADV_GO_LANG/05_Basic_Defer/main.go
+++ 031_ADV_GO_LANG/05_Basic_Defer/main.go
@@ -4,4 +4,15 @@ func main() {
 
 	// defer resp.body.close()
 
+		fmt.Println("Case 1: success")
+	if err := doWork(true); err != nil {
+		fmt.Println("error:", err)
+	}
+
+	fmt.Println("Case 2: Fail Early")
+	if err := doWork(false); err != nil {
+		fmt.Println("error:", err)
+	}
+	
+
 }
