13d240a23d20bb8fdd05085ab1d740d6d029dc5b
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-18 17:33:07 +0530

    Update main.go

--- 031_ADV_GO_LANG/06_Pointers/main.go
+++ 031_ADV_GO_LANG/06_Pointers/main.go
@@ -8,4 +8,11 @@ func main() {
 
 	// *p -> dereferencing (go to that address and read.write)
 
+	// We use pointer because we want to change any value in function and don't want to return it.
+
+}
+
+
+func addScore(score int){
+	
 }
