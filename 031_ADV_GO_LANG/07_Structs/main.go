0c6a989ef88197db6e01e53d63a86b312c42dba3
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-18 17:45:09 +0530

    Update main.go

--- 031_ADV_GO_LANG/07_Structs/main.go
+++ 031_ADV_GO_LANG/07_Structs/main.go
@@ -1,9 +1,12 @@
 package main
+// struct groups related fields into one type
 
-type User struct{
-	
+type User struct {
+	ID    int
+	Name  string
+	Email string
+	Age   int
 }
-
 func main() {
 
 }
