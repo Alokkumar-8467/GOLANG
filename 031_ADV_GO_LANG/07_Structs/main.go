f34d8bd28553f46b3e10fec7b0183c36be52e602
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-18 17:50:24 +0530

    Update main.go

--- 031_ADV_GO_LANG/07_Structs/main.go
+++ 031_ADV_GO_LANG/07_Structs/main.go
@@ -26,4 +26,21 @@ func main() {
 	fmt.Println("Individual Struct value Only Email", u1.Email)
 	fmt.Println("Individual Struct value Only Age", u1.Age)
 
+	/*
+		Full Struct value {11 Alok alok@gmail.com 25}
+		Individual Struct value Only ID 11
+		Individual Struct value Only Name Alok
+		Individual Struct value Only Email alok@gmail.com
+		Individual Struct value Only Age 25
+	*/
+
+	// struct fields are mutable bydefault
+	// Bsically we can change the value if required.
+
+	u1.Age = 400
+
+	fmt.Println("Struct value after mutation", u1)
+
+	// Output = Struct value after mutation {11 Alok alok@gmail.com 400}
+
 }
