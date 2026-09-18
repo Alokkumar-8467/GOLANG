2c35b46630511c2139c46636253dceff80daf339
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-18 17:18:28 +0530

    Update main.go

--- 031_ADV_GO_LANG/05_Basic_Defer/main.go
+++ 031_ADV_GO_LANG/05_Basic_Defer/main.go
@@ -1,10 +1,15 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
 func main() {
 
 	// defer resp.body.close()
 
-		fmt.Println("Case 1: success")
+	fmt.Println("Case 1: success")
 	if err := doWork(true); err != nil {
 		fmt.Println("error:", err)
 	}
@@ -13,30 +18,27 @@ func main() {
 	if err := doWork(false); err != nil {
 		fmt.Println("error:", err)
 	}
-	
 
 }
-
 
 func doWork(success bool) error {
 	// resource related
 	// start message -> resource acquired
 	// cleanup message -> resource released
 
-
-		fmt.Println("start: resource acquired")
+	fmt.Println("start: resource acquired")
 
 	// this defer will guranntee this runs at the end of the func
 	// it return both the path at success as well as for errors.
 	defer fmt.Println("cleanup: resource released")
 
-		if !success {
+	if !success {
 		return errors.New("Something went wrong. i am returning early.")
 	}
 
-		fmt.Println("work: doing something imp")
+	fmt.Println("work: doing something imp")
 	fmt.Println("work: this work is done")
 
-		return nil
+	return nil
 
 }
