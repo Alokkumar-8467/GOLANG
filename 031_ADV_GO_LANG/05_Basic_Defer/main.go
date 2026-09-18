8870bd15cc3a74860e641e632c4d23a59d4330a8
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-18 17:18:01 +0530

    Update main.go

--- 031_ADV_GO_LANG/05_Basic_Defer/main.go
+++ 031_ADV_GO_LANG/05_Basic_Defer/main.go
@@ -29,3 +29,7 @@ func doWork(success bool) error {
 	// this defer will guranntee this runs at the end of the func
 	// it return both the path at success as well as for errors.
 	defer fmt.Println("cleanup: resource released")
+
+		if !success {
+		return errors.New("Something went wrong. i am returning early.")
+	}
