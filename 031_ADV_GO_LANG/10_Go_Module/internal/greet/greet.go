c34890dd85197b4da884ab0d1cf9444376447a34
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-19 17:18:29 +0530

    Update greet.go

--- 031_ADV_GO_LANG/10_Go_Module/internal/greet/greet.go
+++ 031_ADV_GO_LANG/10_Go_Module/internal/greet/greet.go
@@ -1,12 +1,14 @@
 package greet
 
 import "strings"
+
 // If we use Capital Letter then it is Exported function.
 func Hello(name string) string {
 	clean := normalizeName(name)
 
 	return "Hello," + clean
 }
+
 func normalizeName(name string) string {
 	n := strings.TrimSpace(name)
 
