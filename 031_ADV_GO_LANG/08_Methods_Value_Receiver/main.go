264238d28335a05a05ed44a36b56ca38d9824454
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-19 16:37:57 +0530

    Update main.go

--- 031_ADV_GO_LANG/08_Methods_Value_Receiver/main.go
+++ 031_ADV_GO_LANG/08_Methods_Value_Receiver/main.go
@@ -26,3 +26,19 @@ Intro is a method.
 User is the struct type.
 The user is handed to the method, so Intro can use its data.
 Correction 1: the receiver is not a normal input
+
+
+(u User) is called the receiver. It is not listed with the normal inputs. It is the value you call the method on:
+
+go
+u.Intro()   // u is passed in as the receiver
+
+A regular function would take the user as an argument:
+
+go
+func Intro(u User) string { ... }
+Intro(u)
+
+The method version has the same effect. Go just lets you write u.Intro() instead.
+
+Correction 2: it doesn't return u.Name
