883e46e98b56bf738b9c2fbdeaddd99454815e88
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-19 17:18:00 +0530

    Update greet.go

--- 031_ADV_GO_LANG/10_Go_Module/internal/greet/greet.go
+++ 031_ADV_GO_LANG/10_Go_Module/internal/greet/greet.go
@@ -1,3 +1,9 @@
 package greet
 
 import "strings"
+// If we use Capital Letter then it is Exported function.
+func Hello(name string) string {
+	clean := normalizeName(name)
+
+	return "Hello," + clean
+}
