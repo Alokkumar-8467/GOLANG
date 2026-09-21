58abd2681f7d0709426f60f357e917b352276c12
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-21 12:37:51 +0530

    Update main.go

--- 031_ADV_GO_LANG/11_Basic_Http/01_Basic_Http_Server/main.go
+++ 031_ADV_GO_LANG/11_Basic_Http/01_Basic_Http_Server/main.go
@@ -1,1 +1,30 @@
-package basichttpserver
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Response){
+	r.Method  != http.MethodGet {
+		http.Error(w, "only Get is allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	_, _ = w.write([]byte("Hello from GO net/http server"))
+
+}
+
+func main() {
+
+	// http
+	// How to register a router.
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Try going to 8000 port")
+
+	err := http.ListenAndServe(":8080", nil)
+
+	fmt.Println(err)
+
+}
