3cc51bcdb41dca2b52178ee2c21f177992fafc63
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-22 23:17:31 +0530

    Update main.go

--- 031_ADV_GO_LANG/11_Basic_Http/03_JSON_encoder_encode_details/main.go
+++ 031_ADV_GO_LANG/11_Basic_Http/03_JSON_encoder_encode_details/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func successHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-		res := map[string]any{
+	res := map[string]any{
 		"ok":       true,
 		"message":  "Json encode succesfully",
 		"datetime": time.Now().UTC(),
