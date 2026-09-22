a2b05c6a827e42abc1caae18e61af12831b57f25
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-22 22:38:16 +0530

    Update main.go

--- 031_ADV_GO_LANG/11_Basic_Http/02_Http_Multiple_Routes/main.go
+++ 031_ADV_GO_LANG/11_Basic_Http/02_Http_Multiple_Routes/main.go
@@ -2,6 +2,10 @@ package main
 
 import "net/http"
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	_, _ = w.Write([]byte("Welcome try to /hello?name=alok"))
+}
+
 func main() {
 
 	err := http.ListenAndServe()
