ff5c7767eabe53c3bf8f0c36e19bf0a84be1aba4
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-22 22:50:08 +0530

    Update main.go

--- 031_ADV_GO_LANG/11_Basic_Http/03_JSON_encoder_encode_details/main.go
+++ 031_ADV_GO_LANG/11_Basic_Http/03_JSON_encoder_encode_details/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func successHandler()
+
 func main() {
+
+	http.HandleFunc("/ok", successHandler)
+
+	err := http.ListenAndServe(":5000", nil)
+	fmt.Println(err)
 
 }
