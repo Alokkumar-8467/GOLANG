6581cba0de578a14c7cf7eb75677520758a83da4
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-22 23:37:33 +0530

    Update main.go

--- 031_ADV_GO_LANG/11_Basic_Http/04_JSON_decoder_decode_details/main.go
+++ 031_ADV_GO_LANG/11_Basic_Http/04_JSON_decoder_decode_details/main.go
@@ -6,6 +6,12 @@ import (
 	"net/http"
 )
 
+func writeJSON(w http.ResponseWriter, status int, data any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_ = json.NewEncoder(w).Encode(data)
+}
+
 
 func main(){
 	
