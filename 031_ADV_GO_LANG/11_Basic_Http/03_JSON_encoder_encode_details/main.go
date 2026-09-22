2f1c16e8668ddfd0c83508c803df36a611ed497e
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-22 23:17:18 +0530

    Update main.go

--- 031_ADV_GO_LANG/11_Basic_Http/03_JSON_encoder_encode_details/main.go
+++ 031_ADV_GO_LANG/11_Basic_Http/03_JSON_encoder_encode_details/main.go
@@ -9,6 +9,14 @@ func successHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
+		res := map[string]any{
+		"ok":       true,
+		"message":  "Json encode succesfully",
+		"datetime": time.Now().UTC(),
+	}
+	_ = json.NewEncoder(w).Encode(res)
+}
+
 func main() {
 
 	http.HandleFunc("/ok", successHandler)
