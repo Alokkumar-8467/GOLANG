016afa381938640b914ff8bdf905fb5978376729
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-25 12:04:00 +0530

    Update main.go

--- 031_ADV_GO_LANG/11_Basic_Http/06_Reading_Response_Body/main.go
+++ 031_ADV_GO_LANG/11_Basic_Http/06_Reading_Response_Body/main.go
@@ -6,3 +6,13 @@ func main() {
 func main() {
 
 	url := "http://jsonplaceholder.typicode.com/todos"
+	resp, err := http.Get(url)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+}
