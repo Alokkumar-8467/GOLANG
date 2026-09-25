fb0c2acee9ee73ab5abef783315aceb4d574b3ca
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-25 17:43:07 +0530

    Update main.go

--- 031_ADV_GO_LANG/11_Basic_Http/08_External_API/main.go
+++ 031_ADV_GO_LANG/11_Basic_Http/08_External_API/main.go
@@ -14,9 +14,12 @@ type CatFactResponse struct {
 }
 
 
-func writeJSON(w http.ResponseWriter, r *http.Request) {
+func writeJSON(w http.ResponseWriter, status int, data any) {
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_ = json.NewEncoder(w).Encode(data)
 }
+
 
 
 func externalHandler(w http.ResponseWriter, r *http.Request) {
