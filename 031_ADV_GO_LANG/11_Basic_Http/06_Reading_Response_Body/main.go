3ee640b30e686567849b0bd5838e0d490f9f4b50
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-25 12:11:47 +0530

    Update main.go

--- 031_ADV_GO_LANG/11_Basic_Http/06_Reading_Response_Body/main.go
+++ 031_ADV_GO_LANG/11_Basic_Http/06_Reading_Response_Body/main.go
@@ -15,4 +15,7 @@ func main() {
 
 	defer resp.Body.Close()
 
+		// Here we check the status code, if status code is OK or 200 then proceide otherwise give the error with that status code.
+	if resp.StatusCode != http.StatusOK {
+
 }
